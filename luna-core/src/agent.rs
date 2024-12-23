6442af542554d40c570754b4115018ae320005db
siza12 <mattciarrocco31@gmail.com>
2024-12-23 00:47:25 -0500

    Create agent.rs

---
+++ luna-core/src/agent.rs
@@ -0,0 +1,40 @@
+use rig::{agent::AgentBuilder, completion::CompletionModel, embeddings::EmbeddingModel};
+use tracing::info;
+
+use crate::{character::Character, knowledge::KnowledgeBase};
+
+#[derive(Clone)]
+pub struct Agent<M: CompletionModel, E: EmbeddingModel + 'static> {
+pub character: Character,
+completion_model: M,
+knowledge: KnowledgeBase<E>,
+}
+
+impl<M: CompletionModel, E: EmbeddingModel> Agent<M, E> {
+pub fn new(character: Character, completion_model: M, knowledge: KnowledgeBase<E>) -> Self {
+info!(name = [character.name](http://character.name/), "Creating new agent");
+
+```
+    Self {
+        character,
+        completion_model,
+        knowledge,
+    }
+}
+
+pub fn builder(&self) -> AgentBuilder<M> {
+    let builder = AgentBuilder::new(self.completion_model.clone())
+        .preamble(&self.character.preamble)
+        .context(&format!("Your name: {}", self.character.name))
+        .dynamic_context(2, self.knowledge.clone().document_index());
+
+    builder
+}
+
+pub fn knowledge(&self) -> &KnowledgeBase<E> {
+    &self.knowledge
+}
+
+```
+
+}
