0c63fd12222736899a33f1bb4a7ef246307a7ad5
siza12 <mattciarrocco31@gmail.com>
2025-01-04 17:23:58 -0500

    Update lib.rs

--- luna-core/src/lib.rs
+++ luna-core/src/lib.rs
@@ -3,7 +3,7 @@ tracing_subscriber::fmt()
 .with_env_filter(
 tracing_subscriber::EnvFilter::from_default_env()
 .add_directive(tracing::Level::DEBUG.into())
-.add_directive("lexi=debug".parse().unwrap())
+.add_directive("luna=debug".parse().unwrap())
 .add_directive("rustls=off".parse().unwrap())
 .add_directive("hyper=off".parse().unwrap())
 .add_directive("h2=off".parse().unwrap())
